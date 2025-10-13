b65b786481286e4e6471f3fa9ed3512fe74aaa9e
syedmhamil <syedmahamil56@gmail.com>
2025-10-13 16:32:03 +0500

    agent

---
+++ agent-as-tool/agent_as_tool.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+
+from agents import Agent,trace,AsyncOpenAI,OpenAIChatCompletionsModel,RunContextWrapper, Runner,set_tracing_disabled, function_tool
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+gemini_api_key: str | None = os.environ.get("GOOGLE_API_KEY")
+
+set_tracing_disabled(disabled=False)
+external_client: AsyncOpenAI = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+
+llm_provider : OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(model='gemini-2.5-flash', openai_client=external_client) 
+    
+spanish_Agent = Agent(name="Spanish translator",model=llm_provider, instructions="Translate the input text to Spanish.")
+french_Agent = Agent(name="French translator",model=llm_provider, instructions="Translate the input text to French.")
+
+agent = Agent(name="Orchestration",model=llm_provider,tools=[spanish_Agent.as_tool( tool_name="translate_to_spanish",
+            tool_description="Translate the user's message to Spanish.") , french_Agent.as_tool( tool_name="translate_to_french",
+            tool_description="Translate the user's message to French.")], instructions="Decide whether to translate the input text to Spanish or French based on the user's preference. Use the appropriate tool to perform the translation.", reset_tool_choice= )
+
+
+async def main():
+    with trace("Translation workflow"): 
+        result = await Runner.run(agent, "Translate 'Hello, how are you?' to French." )
+        print(f"Final Output: {result.final_output}")
+
+
+asyncio.run(main())
