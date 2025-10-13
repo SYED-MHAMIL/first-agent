9c8ad5aa58d804ef7701b4f1d43686e575b0c198
syedmhamil <syedmahamil56@gmail.com>
2025-10-13 22:24:53 +0500

    agent

--- agent-as-tool/agent-tool.py
+++ agent-as-tool/agent-tool.py
@@ -8,6 +8,7 @@ load_dotenv(find_dotenv())
 gemini_api_key: str | None = os.environ.get("GOOGLE_API_KEY")
 
 set_tracing_disabled(disabled=False)
+
 external_client: AsyncOpenAI = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
 
 llm_provider : OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(model='gemini-2.5-flash', openai_client=external_client) 
@@ -31,11 +32,12 @@ teacher_agent = Agent(
     name="Teacher",
     instructions="Help students write clearly. Use tools when asked to fix text.",
     tools=[proofread_text],
+    model= llm_provider 
 )
 
 async def main():
     with trace("Proofreading workflow"): 
-        result = await Runner.run(teacher_agent, "Please proofread this sentence: 'She dont know how to do it right'")
+        result = await Runner.run(teacher_agent, "Please proofread this sentence: 'She dont know how to do it right'", max_turns=3)
         print(f"Final Output: {result.final_output}")
 
 
