2c7b31b72f7af8ec54c539b5cdac9d3f7aaa97bb
syedmhamil <syedmahamil56@gmail.com>
2025-10-14 12:33:39 +0500

    agent-tool

--- agent-as-tool/agent-tool.py
+++ agent-as-tool/agent-tool.py
@@ -6,7 +6,7 @@ import os
 load_dotenv(find_dotenv())
 gemini_api_key: str | None = os.environ.get("GOOGLE_API_KEY")
 
-# set_tracing_disabled(disabled=False)
+set_tracing_disabled(disabled=True)
 
 external_client: AsyncOpenAI = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
 llm_provider : OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(model='gemini-2.5-flash', openai_client=external_client) 
