37d0fe49a6855f5e207719c4288a447538a7123a
syedmhamil <syedmahamil56@gmail.com>
2025-10-17 17:05:14 +0500

    agent

--- 22_memory_management/embedding_and_vectorDB/README.py
+++ 22_memory_management/embedding_and_vectorDB/README.py
@@ -1,3 +1,4 @@
+import os
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 import faiss
@@ -5,7 +6,8 @@ import numpy as np
 
 load_dotenv(find_dotenv())
 # 1. Get embeddings from OpenAI
-client = OpenAI()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+client = OpenAI(api_key=GOOGLE_API_KEY)
 
 texts = ["dog", "puppy", "car"]
 embeddings  = [client.embeddings.create(model='text-embedding-3-small' ,input=t) for t in texts]
