061c3ee57c577d7588819edb635e9169bed7d6c5
syedmhamil <syedmahamil56@gmail.com>
2025-10-19 15:13:44 +0500

    agent

--- 22_memory_management/embedding_and_vectorDB/README.py
+++ 22_memory_management/embedding_and_vectorDB/README.py
@@ -1,22 +1,19 @@
+from openai import OpenAI
 import os
 from dotenv import load_dotenv, find_dotenv
 import faiss
-import google.generativeai as genai
 import numpy as np
 
 
 load_dotenv(find_dotenv())
-GEMINI_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-genai.configure(api_key=GEMINI_API_KEY)
-
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client =  OpenAI(api_key=OPENAI_API_KEY)
 
 
 # 1. Set embeddings from OpenAI
 
 texts = ["dog", "puppy", "car"]
-embeddings  = [genai.embed_content(model="models/embedding-001", content=t)["embedding"] for t in texts]
+embeddings  = [client.embeddings.create(model="text-embedding-3-small", input=t).data[0].embedding  for t in texts]
 dimentions = len(embeddings)
 print(embeddings)
 
