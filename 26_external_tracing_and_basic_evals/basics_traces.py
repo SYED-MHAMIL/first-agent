aa967ee0fb1ff7c2e6534c5020ad9f9184805776
syedmhamil <syedmahamil56@gmail.com>
2025-10-24 17:09:31 +0500

    langduse

--- 26_external_tracing_and_basic_evals/basics_traces.py
+++ 26_external_tracing_and_basic_evals/basics_traces.py
@@ -1,58 +1,58 @@
-# import asyncio
-# import os
-# from dotenv import load_dotenv, find_dotenv
-# from agents import Agent,Runner,set_default_openai_api,set_default_openai_client,set_tracing_export_api_key
-# from openinference.instrumentation.openai_agents import OpenAIAgentsInstrumentor
-# from openai import AsyncOpenAI 
-# from langfuse import get_client
+import asyncio
+import os
+from dotenv import load_dotenv, find_dotenv
+from agents import Agent,Runner,set_default_openai_api,set_default_openai_client,set_tracing_export_api_key
+from openinference.instrumentation.openai_agents import OpenAIAgentsInstrumentor
+from openai import AsyncOpenAI 
+from langfuse import get_client
 
 
 
 
 
-# # load  env
-# load_dotenv(find_dotenv())
-# gemini_api_key: str | None = os.environ.get("GOOGLE_API_KEY")
-# os.getenv("LANGFUSE_SECRET_KEY")
-# os.getenv("LANGFUSE_PUBLIC_KEY")
-# os.getenv("LANGFUSE_HOST")
+# load  env
+load_dotenv(find_dotenv())
+gemini_api_key: str | None = os.environ.get("GOOGLE_API_KEY")
+os.getenv("LANGFUSE_SECRET_KEY")
+os.getenv("LANGFUSE_PUBLIC_KEY")
+os.getenv("LANGFUSE_HOST")
 
 
-# external_client: AsyncOpenAI = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/") 
-# set_default_openai_client(external_client,use_for_tracing=False)
-# set_default_openai_api("chat_completions")
+external_client: AsyncOpenAI = AsyncOpenAI(api_key=gemini_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/") 
+set_default_openai_client(external_client,use_for_tracing=False)
+set_default_openai_api("chat_completions")
 
 
-# # get the logs and traces
+# get the logs and traces
 
-# OpenAIAgentsInstrumentor().instrument() 
+OpenAIAgentsInstrumentor().instrument() 
 
-# # Verify connection
+# Verify connection
 
-# langfuse =  get_client()
+langfuse =  get_client()
 
-# if langfuse.auth_check():
-#     print("✅ Langfuse client is authenticated and ready!")
-# else:
-#     print("❌ Authentication failed. Please check your credentials and host.")
+if langfuse.auth_check():
+    print("✅ Langfuse client is authenticated and ready!")
+else:
+    print("❌ Authentication failed. Please check your credentials and host.")
     
 
 
-# async def main():
-#     """Run an AI agent that replies in haikus."""
-#     agent = Agent(
-#         name="Assistant",
-#         instructions="You only respond in haikus.",
-#         model = "gemini-2.5-flash",
-#     )
+async def main():
+    """Run an AI agent that replies in haikus."""
+    agent = Agent(
+        name="Assistant",
+        instructions="You only respond in haikus.",
+        model = "gemini-2.5-flash",
+    )
 
-#     result = await Runner.run(agent, "Tell me about recursion in programming.")
-#     print("\n--- Agent Response ---")
-#     print(result.final_output)
+    result = await Runner.run(agent, "Tell me about recursion in programming.")
+    print("\n--- Agent Response ---")
+    print(result.final_output)
 
 
-# # -----------------------------
-# # Entry point
-# # -----------------------------
-# if __name__ == "__main__":
-#     asyncio.run(main())
+# -----------------------------
+# Entry point
+# -----------------------------
+if __name__ == "__main__":
+    asyncio.run(main())
