72d143839830ce9ca6368053cb8b3826dd2baa3d
syedmhamil <syedmahamil56@gmail.com>
2025-11-03 12:52:33 +0500

    agent

--- 26_external_tracing_and_basic_evals/04_grouping_agent_runs_trace.py
+++ 26_external_tracing_and_basic_evals/04_grouping_agent_runs_trace.py
@@ -58,7 +58,7 @@ async def main():
     )
 
     with trace("Joke workflow Generator"):
-        first_result = await Runner.run(agent, "Tell me a joke about dog" \
+        first_result = await Runner.run(agent, "Tell me a joke about dog" 
         "")
         second_result = await Runner.run(agent, f"Rate this joke: {first_result.final_output}")
         print(f"Joke: {first_result.final_output}")
