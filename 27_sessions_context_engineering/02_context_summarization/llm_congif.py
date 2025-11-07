01969a80e6fb62b2bfe552f85e481bd1117ba018
syedmhamil <syedmahamil56@gmail.com>
2025-11-07 17:52:21 +0500

    serve

---
+++ 27_sessions_context_engineering/02_context_summarization/llm_congif.py
@@ -0,0 +1,27 @@
+import os   
+from  openai import AsyncOpenAI
+from agents import Agent ,OpenAIChatCompletionsModel
+from dotenv import  load_dotenv
+
+
+load_dotenv()
+
+#  ============================================================ 
+
+def set_gimini_model():
+    """Configure gimini model using openAI-compatible API"""
+    api_key  = os.getenv("GOOGLE_API_KEY")
+    external_client = AsyncOpenAI(
+        api_key=api_key,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
+    
+    return  OpenAIChatCompletionsModel(
+         model="gemini-2.5-flash",
+         openai_client= external_client   
+    )   
+
+llm_model  = set_gimini_model()
+based_agent = Agent(
+        name =  "Base Agent", 
+        model = llm_model
+)
